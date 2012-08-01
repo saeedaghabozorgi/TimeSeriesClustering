fa9be1c1094216e2b222b28877877288cdd8986a
saghabozorgi@gmail.com <saghabozorgi@gmail.com@0a64c8b3-e7c0-53e5-5281-12a358f43c15>
2012-08-01 04:51:10 +0000

    Adding parallel calculation

--- clustering_ds.m
+++ clustering_ds.m
@@ -4,16 +4,16 @@ train_data = importdata(file_name);
 lable_file_name=['..\Datasets\Face(all)\Label_FaceAll.mat'];
 train_data_lable = importdata(lable_file_name);
 %-----------to min
-d_data=[];
-lab=[];
-for i=1:14
-    x=find(train_data_lable==i);
-    x=x(1:5);
-    d_data=[d_data;train_data(x,:)];
-    lab=[lab;train_data_lable(x,:)];
-end
-train_data=d_data;
-train_data_lable=lab;
+% d_data=[];
+% lab=[];
+% for i=1:14
+%     x=find(train_data_lable==i);
+%     x=x(1:2);
+%     d_data=[d_data;train_data(x,:)];
+%     lab=[lab;train_data_lable(x,:)];
+% end
+% train_data=d_data;
+% train_data_lable=lab;
 %-----------------
 
 % train_data_lable=train_data_lable(p,:);
@@ -25,8 +25,42 @@ data_len= size(train_data,2);
 
 %% clustering
 %-----------------------------
-details(1,:)=clustering_Hybrid_3Level(nor_traj,k,p);
+
+paralele_DTW(nor_traj);
+%-----------------------------
+%details(1,:)=clustering_Hybrid_3Level(nor_traj,k,p);
 %-----------------------------
 % parameter1={'l1_dis_method','DTW','l1_dtw_bound',1,'l1_rep','RAW','l1_alphabet_size',8,'l1_compression_ratio',2,'l1_alg','k-medoids-keogh'};
 % c=clustering_l1_preclustering(k,p,nor_traj,parameter1{:});
+end
+
+function paralele_DTW(nor_traj)
+poolSize = matlabpool('size');
+if poolSize == 0
+   error('parallel:demo:poolClosed', ...
+        'This demo needs an open MATLAB pool to run.');
+end
+fprintf('This demo is running on %d MATLABPOOL workers.\n', ...
+    matlabpool('size'));
+
+x=nor_traj;
+data_n = length(x);
+dismat=zeros(data_n,data_n);
+parfor  i = 1:data_n
+    disp(num2str(i));
+    dismat(i,:)=rowcalc(i,data_n,x);
+end
+save('dismat_DTW_FaceAll.mat', 'dismat')
+end
+
+function dismatrix=rowcalc(row,data_n,x)
+dismatrix=zeros(1,data_n);
+    a=x{row};
+    for j = row:data_n,
+        if row ~= j
+            b=x{j};
+            dis=dis_dtw3(a,b,size(a,2));
+            dismatrix(1, j) =dis;
+        end
+    end
 end
